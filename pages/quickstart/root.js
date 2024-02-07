30d208e6334022b428e2bbeb6eea5c9ac1670baa
AndiAxiologic <113915706+AndiAxiologic@users.noreply.github.com>
2024-02-07 14:09:00 +0200

    update root 2

--- pages/quickstart/root.js
+++ pages/quickstart/root.js
@@ -1,12 +1,13 @@
-![Image](https://docs.google.com/drawings/d/e/2PACX-1vTkMe3YMQ6rziFepuCpvRWBQXiRRnkKUHyO5sF08wBR04VuxCStRf_p0t7G-eqfVcDPxIcD_F8qkdoC/pub?w=1210&h=615){:class="imgMain" onclick="openModal(this.src)"}
+// root.js
 
-## Caption
+function openModal(imgSrc) {
+  var modal = document.getElementById("myModal");
+  var modalImg = document.getElementById("modalImg");
+  modal.style.display = "block";
+  modalImg.src = imgSrc;
+}
 
-Some text here.
-
-<div id="myModal" class="modal">
-  <span class="close" onclick="closeModal()">&times;</span>
-  <img class="modal-content" id="modalImg">
-</div>
-
-<script src="root.js"></script>
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
